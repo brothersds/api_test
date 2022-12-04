c56c0857a16c0601acb511d4708199aa372049c8
Dmitry <brothersds@ngs.ru>
2022-12-04 20:46:47 +0300

    end chuck_1

--- chuck.py
+++ chuck.py
@@ -2,3 +2,12 @@ import requests
 
 url = "https://api.chucknorris.io/jokes/random"
 print(url)
+result = requests.get(url)
+print("Статус код :" + str(result.status_code))
+assert 200 == result.status_code
+if result.status_code == 200:
+    print("Успешно!!! Мы получили новую шутку!")
+else:
+    print("Провал!!! Запрос ошибочный")
+result.encoding = 'utf-8'
+print(result.text)
