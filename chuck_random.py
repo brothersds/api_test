d1517eea47c66d885090e2003f2bfe6a42482473
Dmitry <brothersds@ngs.ru>
2022-12-04 21:24:45 +0300

    begin chuck_class_2

--- chuck_random.py
+++ chuck_random.py
@@ -7,8 +7,35 @@ class TestNewJoke():
     def __int__(self):
         pass
 
-    def test_create_new_random_joke(self):
-        """Создание случайной шутки"""
+    # def test_create_new_random_joke(self):
+    #     """Создание случайной шутки"""
+    #     url = "https://api.chucknorris.io/jokes/random"
+    #     print(url)
+    #     result = requests.get(url)
+    #     print("Статус код :" + str(result.status_code))
+    #     assert 200 == result.status_code
+    #     if result.status_code == 200:
+    #         print("Успешно!!! Мы получили новую шутку!")
+    #     else:
+    #         print("Провал!!! Запрос ошибочный")
+    #     result.encoding = 'utf-8'
+    #     print(result.text)
+    #     check = result.json()
+    #     # check_info = check.get("categories")
+    #     # print(check_info)
+    #     # print(result.json().get())
+    #     # assert check_info == []
+    #     # print("Категория верна")
+    #     check_info_value = check.get("value")
+    #     print(check_info_value)
+    #     name = "Chuck"
+    #     if name in check_info_value:
+    #         print("Chuck  присутствует")
+    #     else:
+    #         print("Chuck  отсутствует")
+
+    def test_create_new_random_categories_joke(self):
+        """Создание случайной шутки на определенную тему"""
         url = "https://api.chucknorris.io/jokes/random"
         print(url)
         result = requests.get(url)
